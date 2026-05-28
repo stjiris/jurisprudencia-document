15a28b989a5481d17e03dad97b9775abd05ca3b3
vascofelix <vascorfelix@tecnico.ulisboa.pt>
2026-05-28 09:44:29 +0100

    feat: added v14 with pdf field

--- src/jurisprudencia-document.ts
+++ src/jurisprudencia-document.ts
@@ -1,4 +1,4 @@
-export const JurisprudenciaVersion = "jurisprudencia.13.0"
+export const JurisprudenciaVersion = "jurisprudencia.14.0"
 
 type NullableField<F> = F | null | undefined;
 type N<F> = NullableField<F>;
@@ -102,6 +102,7 @@ export const JurisprudenciaDocumentProperties = {
     "Fonte": ExactFieldMapping,
     "URL": ExactFieldMapping,
     "UUID": ExactFieldMapping,
+    "PDF": ExactFieldMapping,
     "HASH": {
         type: "object",
         properties: {
@@ -132,7 +133,7 @@ export const JurisprudenciaDocumentHashKeys = ["HASH"] as const;
 export const JurisprudenciaDocumentObjectKeys = ["Original"] as const;
 export const JurisprudenciaDocumentDateKeys = ["Data"] as const;
 export const JurisprudenciaDocumentTextKeys = ["Sumário", "Texto", "Sumário Não Anonimizado", "Texto Não Anonimizado"] as const;
-export const JurisprudenciaDocumentExactKeys = ["Número de Processo", "ECLI", "Fonte", "URL", "UUID", "Tipo"] as const;
+export const JurisprudenciaDocumentExactKeys = ["Número de Processo", "ECLI", "Fonte", "URL", "UUID", "Tipo", "PDF"] as const;
 export const JurisprudenciaDocumentGenericKeys = ["Relator Nome Profissional", "Relator Nome Completo", "Descritores", "Meio Processual", "Votação", "Secção", "Área", "Decisão", "Tribunal de Recurso", "Tribunal de Recurso - Processo", "Área Temática", "Jurisprudência Estrangeira", "Jurisprudência Internacional", "Jurisprudência Nacional", "Doutrina", "Legislação Comunitária", "Legislação Estrangeira", "Legislação Nacional", "Referências Internacionais", "Indicações Eventuais", "Referência de publicação", "Jurisprudência"] as const;
 
 export type JurisprudenciaDocumentStateKey = typeof JurisprudenciaDocumentStateKeys[number];
